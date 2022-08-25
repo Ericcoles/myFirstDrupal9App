e029566308557c3f5c57afc90233a65821a0006d
Coles Eric <s2200118@BC-C02GR0BJQ6LR-MacBook-Air.local>
2022-08-25 13:59:21 +0300

    created two custom modules first_module and coles_module along with some small other updates

---
+++ modules/custom/coles_module/src/Controller/colesController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drupal\coles_module\Controller;
+
+class ColesController {
+  public function coles() {
+    return array('#markup' => 'Welcome to Cool Coles Drupal 9 websity Amigo, you dig?');
+
+  }
+
+}
