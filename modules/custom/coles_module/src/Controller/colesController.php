7af52a7f86c92520a808a39a34cc5805f026d26d
Coles Eric <s2200118@BC-C02GR0BJQ6LR-MacBook-Air.local>
2022-08-25 14:32:03 +0300

    created two custom modules first_module and coles_module along with some small other updates

--- modules/custom/coles_module/src/Controller/colesController.php
+++ modules/custom/coles_module/src/Controller/colesController.php
@@ -2,10 +2,14 @@
 
 namespace Drupal\coles_module\Controller;
 
-class ColesController {
+use \Drupal\core\Controller\ControllerBase;
+// use \Drupal\Core\StringTranslation\StringTranslationTrait;
+
+class ColesController extends ControllerBase {
   public function coles() {
-    return array('#markup' => 'Welcome to Cool Coles Drupal 9 websity Amigo, you dig?');
-
+    // return array('#markup' => 'Welcome to Cool Coles Drupal 9 websity Amigo, you dig?');
+    return ['#markup' =>$this->t('Welcome to My drupal') ];
   }
-
 }
+
+/// single thing but controllerbase includes all of them like log ect.Controller base is King!
--- modules/custom/first_module/src/Controller/firstController.php
+++ modules/custom/first_module/src/Controller/firstController.php
@@ -2,10 +2,18 @@
 
 namespace Drupal\first_module\Controller; 
 
+use \Drupal\Core\StringTranslation\StringTranslationTrait;
+
 class FirstController {
+
+  use StringTranslationTrait;
+
   public function first() {
     return array('#markup'=> 'Welcome to drupal website amigo :)');
 
   }
 
 }
+
+
+/// single thing but controllerbase includes all of them like log ect.
